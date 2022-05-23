f0d9ba04ea84252de5378e57ab244a448ff9d2e6
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2022-05-23 19:55:49 -0400

    Methods and process (add,substract,multiplicateBy,DivideBy)

---
+++ Kata1/Temperature.py
@@ -0,0 +1,99 @@
+def __toFarenheit__(grade, scale):
+    if scale == "C":
+        F = (grade * (9 / 5)) + 32
+    elif scale == "K":
+        F = 1.8 * (grade - 273.15) + 32
+    else:
+        F = grade
+    return F
+
+
+def __toCelsius__(grade, scale):
+    if scale == "F":
+        C = (grade - 32) * (5 / 9)
+    elif scale == "K":
+        C = (grade - 273.15)
+    else:
+        C = grade
+    return C
+
+
+def __toKelvin__(grade, scale):
+    if scale == "F":
+        K = (grade - 32) * (5 / 9) + 273.15
+    elif scale == "C":
+        K = (grade + 273.15)
+    else:
+        K = grade
+    return K
+
+
+class Temperature:
+
+    def __init__(self, Grade, Scale):
+        self.grade = Grade
+        self.scale = Scale
+
+    def __add__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFarenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.grade = 0
+                self.scale = "No valido"
+            return Temperature(self.grade + result, self.scale)
+        else:
+            return "No Valid"
+
+    def __substract__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFarenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.grade = 0
+                self.scale = "No valido"
+            return Temperature(self.grade - result, self.scale)
+        else:
+            return "No Valid"
+
+    def __multiplicateBy__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFarenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.grade = 0
+                self.scale = "No valido"
+            return Temperature(self.grade * result, self.scale)
+        else:
+            return "No Valid"
+
+    def __divideBy__(self, other):
+        if isinstance(other, Temperature):
+            if self.scale == "F":
+                result = __toFarenheit__(other.grade, other.scale)
+            elif self.scale == "C":
+                result = __toCelsius__(other.grade, other.scale)
+            elif self.scale == "K":
+                result = __toKelvin__(other.grade, other.scale)
+            else:
+                result = 0
+                self.grade = 0
+                self.scale = "No valido"
+            return Temperature(self.grade / result, self.scale)
+        else:
+            return "No Valid"
--- Kata1/main.py
+++ Kata1/main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Temperature import Temperature
+from TemperatureScalar import TemperatureScalar
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def __TemperatureProcess__(grade1, scale1, grade2, scale2, option):
+    v1 = Temperature(grade1, scale1)
+    v2 = Temperature(grade2, scale2)
+
+    v3 = v1.__add__(v2)
+    return f"(Grade: {v3.grade}, Scalar: {v3.scale})"
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    result = __TemperatureProcess__(25, TemperatureScalar.Celsius, 20, TemperatureScalar.Fahrenheit, 1)
+    print(result)
