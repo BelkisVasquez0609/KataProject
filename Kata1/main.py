c8e22289a27c02f2f90d119116c66cc3094b877c
Belkis Vaq <55562967+BelkisVasquez0609@users.noreply.github.com>
2022-05-23 20:02:26 -0400

    options (add,substract,multiplicateBy, divideBy)

--- Kata1/main.py
+++ Kata1/main.py
@@ -6,10 +6,19 @@ def __TemperatureProcess__(grade1, scale1, grade2, scale2, option):
     v1 = Temperature(grade1, scale1)
     v2 = Temperature(grade2, scale2)
 
-    v3 = v1.__add__(v2)
+    if option == 1:  # add
+        v3 = v1.__add__(v2)
+    elif option == 2:  # substract
+        v3 = v1.__substract__(v2)
+    elif option == 3:  # multiplicativeBy
+        v3 = v1.__multiplicateBy__(v2)
+    elif option == 4:  # divideBy
+        v3 = v1.__divideBy__(v2)
+    else:
+        return "No Valid"
     return f"(Grade: {v3.grade}, Scalar: {v3.scale})"
 
 
 if __name__ == '__main__':
-    result = __TemperatureProcess__(25, TemperatureScalar.Celsius, 20, TemperatureScalar.Fahrenheit, 1)
+    result = __TemperatureProcess__(56, TemperatureScalar.Celsius, 20, TemperatureScalar.Fahrenheit, 1)
     print(result)
